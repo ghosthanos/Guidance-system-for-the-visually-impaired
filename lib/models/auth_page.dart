eaf6f42219a840ca4883506fd464938ce7e32e22
vignesh <104735393+ghosthanos@users.noreply.github.com>
2023-05-31 11:59:57 +0530

    Update auth_page.dart

--- lib/models/auth_page.dart
+++ lib/models/auth_page.dart
@@ -16,7 +16,7 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot) {
           // user is logged in
           if (snapshot.hasData) {
-            return MainHomePage();
+            return OpenScreen();
           }
 
           // user is NOT logged in
